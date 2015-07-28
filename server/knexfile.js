d361280848773a9834a59d12352ba8d3e10a06b8
Isto <istobarton@gmail.com>
2015-07-28 17:11:04 -0500

    Connect node server with psql server

--- server/knexfile.js
+++ server/knexfile.js
@@ -5,7 +5,7 @@ module.exports = {
   development: {
     client: 'postgresql',
     connection: {
-      database: 'pg://localhost/fount',
+      database: 'linked_list',
     },
     pool: {
       min: 2,
@@ -19,9 +19,7 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'pg://localhost/fount',
-    },
+    connection:'linked_list',
     pool: {
       min: 2,
       max: 10
