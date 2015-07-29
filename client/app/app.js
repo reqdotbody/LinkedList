b026f32a7c32255fc7f41e55492e0d1ac18a82c2
fmaredia <farhana.maredia@gmail.com>
2015-07-28 19:29:12 -0500

    changes location of app.js

--- client/app/app.js
+++ client/app/app.js
@@ -1,3 +1,1 @@
-// general view structure for the index.html page + navigation bar view
 angular.module('app', [])
-// link to factories and controllers?
--- client/app/index/index.js
+++
@@ -1,3 +0,0 @@
-// general view structure for the index.html page + navigation bar view
-angular.module('app', [])
-// link to factories and controllers?
