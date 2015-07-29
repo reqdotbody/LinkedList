466ed2515842c5bb41b4354c06347c1f8053f26a
fmaredia <farhana.maredia@gmail.com>
2015-07-29 10:13:11 -0500

    ui-router progress

--- client/app/app.js
+++ client/app/app.js
@@ -1,1 +1,51 @@
-angular.module('app', [])
+angular.module('app', ['ui.router'])
+
+.controller("MainController", function($scope){
+  
+})
+
+.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider){
+  
+  $urlRouterProvider.otherwise('/all');
+
+  $stateProvider
+    .state('app', {
+      abstract: true,
+      views: {
+        nav: {
+          templateUrl: '/main/nav.html',
+          controller: 'NavController as Nav'
+        }
+      }
+    })
+    .state('app.signup', {
+      url: '/signup',
+      templateUrl: '/main/signup.html',
+      controller: 'SignupController'
+    })
+    .state('app.login', {
+      url: '/login',
+      templateUrl: '/main/login.html',
+      controller: 'LoginController'
+    })
+    .state('app.logout', {
+      url: '/logout',
+      templateUrl: '/main/allprojects.html',
+      controller: 'AllProjectsController'
+    })
+    .state('app.newproject', {
+      url: '/newproject',
+      templateUrl: '/main/newproject.html',
+      controller: 'NewProjectController'
+    })
+    .state('app.dashboard', {
+      url: '/dashboard',
+      templateUrl: '/dashboard/index.html',
+      controller: 'DashboardController'
+    })
+    .state('app.project', {
+      url: '/project/{id}',
+      templateUrl: '/project/index.html',
+      controller: 'ProjectController'
+    });
+}]);
