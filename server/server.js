00b4309da94fc615654fe80d4238d7c4ec0f2a7e
Isto <istobarton@gmail.com>
2015-07-30 16:27:15 -0500

    Beginnings of deployment

--- server/server.js
+++ server/server.js
@@ -56,7 +56,6 @@ var server = app.listen(process.env.PORT || 3000, function () {
   var host = server.address().address;
   var port = server.address().port;
 
-  console.log('Example app listening at http://%s:%s', host, port);
-
+  console.log('The magic happens at http://%s:%s', host, port);
 
 });
