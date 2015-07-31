10b3109861375856b037295d9435e6fd2a3bba21
Isto <istobarton@gmail.com>
2015-07-31 12:03:11 -0500

    Console Log Latest Migration

--- db/migrations/20150731114840_project_adjustment.js
+++ db/migrations/20150731114840_project_adjustment.js
@@ -2,6 +2,7 @@
 exports.up = function(knex, Promise) {
   var drop_project_name = knex.schema.table('projects', function(table){
     table.dropColumn('name');
+    console.log("We dropped that project name!")
     })
     return Promise.all([drop_project_name]);
 };
