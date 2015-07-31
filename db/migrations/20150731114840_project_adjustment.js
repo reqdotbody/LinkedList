25b9b1a83abf91e3dc535019cb4c8e9c752554fe
Isto <istobarton@gmail.com>
2015-07-31 11:54:15 -0500

    Database Migration Project Name

---
+++ db/migrations/20150731114840_project_adjustment.js
@@ -0,0 +1,11 @@
+
+exports.up = function(knex, Promise) {
+  var drop_project_name = knex.schema.table('projects', function(table){
+    table.dropColumn('name');
+    })
+    return Promise.all([drop_project_name]);
+};
+
+exports.down = function(knex, Promise) {
+  
+};
