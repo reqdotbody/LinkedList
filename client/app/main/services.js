d9cd49fe508fece4e3b627f1422b865bb11bd6ec
ecastano <esteban.rotary@gmail.com>
2015-07-31 09:20:19 -0500

    Finished authentication. Correctly authenticates user. Added logout button/feature.

--- client/app/main/services.js
+++ client/app/main/services.js
@@ -3,7 +3,7 @@ angular.module('app')
 .factory('Auth', function ($http){
 
   var isAuthenticated = function(){
-    console.log("Inside Auth factory");
+    console.log("Inside Auth factory - isAuthenticated");
     //asks the server, "is the current user logged in?"
     //returns true or false (or error);
     return $http({
@@ -13,11 +13,23 @@ angular.module('app')
     .success(function(resp){
       return resp;
     })
-    
+  }
+
+  var logout = function(){
+    console.log("Inside Auth factory - logout");
+    //tells the server to log the user out
+    return $http({
+      method: 'GET',
+      url: '/logout'
+    })
+    .success(function(resp){
+      console.log("User has been logged out");
+    })
   }
 
   return {
-    isAuthenticated: isAuthenticated
+    isAuthenticated: isAuthenticated,
+    logout: logout
   }
 
 });
